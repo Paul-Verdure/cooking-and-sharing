fb84631c17ea29989eaef9bd7d048586d3ffed69
Paul Verdure <paul.verdure@gmail.com>
2024-01-12 17:30:16 +0100

    chore: add tailwind prettier plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'chilean-fire': {
+          '50': '#fffaec',
+          '100': '#fff4d3',
+          '200': '#ffe5a5',
+          '300': '#ffd06d',
+          '400': '#ffb032',
+          '500': '#ff960a',
+          '600': '#f57900',
+          '700': '#cc5c02',
+          '800': '#a1470b',
+          '900': '#823c0c',
+          '950': '#461c04',
+        },
+      },
+    },
   },
   plugins: [],
 }
