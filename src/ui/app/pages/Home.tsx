b470adbc4d231d8fcc5f96e9a2e236f939eec67b
Paul Verdure <paul.verdure@gmail.com>
2024-01-15 17:52:38 +0100

    feat: add hero section and a second one

--- src/ui/app/pages/Home.tsx
+++ src/ui/app/pages/Home.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import home from '../assets/home.png';
+import cooking from '../assets/cooking.png';
 
 function Home() {
 	return (
-		<h1 className="text-4xl font-bold text-center">Home</h1>
+		<>
+			<section className="flex max-h-[600px] items-center justify-center space-x-8 px-8">
+				<div className="flex-column space-y-6">
+					<h2 className="text-wistful-700 text-5xl font-bold">
+						Cooking & Sharing
+					</h2>
+					<p className="max-w-prose text-xl">
+						Welcome to the Cooking & Sharing app! Your go-to platform for
+						sharing and discovering delicious recipes.
+					</p>
+					<div className="space-x-4">
+						<button className="bg-wistful-500 hover:bg-wistful-600 border-wistful-600 active:bg-wistful-700 rounded border-2 px-4 py-2 font-bold text-white">
+							Login
+						</button>
+						<button className="text-wistful-600 border-wistful-600 active:bg-wistful-200 hover: hover:bg-wistful-100 rounded border-2 px-4 py-2 font-bold">
+							Sign Up
+						</button>
+					</div>
+				</div>
+				<div>
+					<img src={home} alt="home" className="max-h-[800px]" />
+				</div>
+			</section>
+			<section className="bg-wistful-200 flex max-h-[600px] items-center space-x-8 px-8">
+				<div>
+					<img src={cooking} alt="cooking" className="max-h-[800px]" />
+				</div>
+				<div className="flex-column space-y-6">
+					<h3 className="text-wistful-700 text-2xl font-bold">
+						Easy recipe creation process
+					</h3>
+					<p className="max-w-prose text-xl">
+						Enjoy hassle-free recipe creation that allows you to unlock your
+						full creativity and focus on what matters.
+					</p>
+				</div>
+			</section>
+		</>
 	);
 }
 
