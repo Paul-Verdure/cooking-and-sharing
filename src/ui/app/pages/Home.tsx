aaa651dec93a099b76cf2e290ff2d60e47035e0d
Paul Verdure <paul.verdure@gmail.com>
2024-01-16 21:39:24 +0100

    fix: images overflow on home

--- src/ui/app/pages/Home.tsx
+++ src/ui/app/pages/Home.tsx
@@ -5,7 +5,7 @@ import cooking from '../assets/cooking.png';
 function Home() {
 	return (
 		<>
-			<section className="flex max-h-[600px] items-center justify-center space-x-8 px-8">
+			<section className="flex mt-20 max-h-[600px] items-center justify-center space-x-8 px-8">
 				<div className="flex-column space-y-6">
 					<h2 className="text-wistful-700 text-5xl font-bold">
 						Cooking & Sharing
@@ -15,21 +15,21 @@ function Home() {
 						sharing and discovering delicious recipes.
 					</p>
 					<div className="space-x-4">
-						<button className="bg-wistful-500 hover:bg-wistful-600 border-wistful-600 active:bg-wistful-700 rounded border-2 px-4 py-2 font-bold text-white">
+						<button className="bg-wistful-500 shadow-md hover:bg-wistful-600 border-wistful-600 active:bg-wistful-700 rounded border-2 px-4 py-2 font-bold text-white">
 							Login
 						</button>
-						<button className="text-wistful-600 border-wistful-600 active:bg-wistful-200 hover: hover:bg-wistful-100 rounded border-2 px-4 py-2 font-bold">
+						<button className="text-wistful-600 shadow-md border-wistful-600 active:bg-wistful-200 hover: hover:bg-wistful-100 rounded border-2 px-4 py-2 font-bold">
 							Sign Up
 						</button>
 					</div>
 				</div>
 				<div>
-					<img src={home} alt="home" className="max-h-[800px]" />
+					<img src={home} alt="home" className="max-h-[600px] max-w-full" />
 				</div>
 			</section>
 			<section className="bg-wistful-200 flex max-h-[600px] items-center space-x-8 px-8">
 				<div>
-					<img src={cooking} alt="cooking" className="max-h-[800px]" />
+					<img src={cooking} alt="cooking" className="max-h-[600px] max-w-full" />
 				</div>
 				<div className="flex-column space-y-6">
 					<h3 className="text-wistful-700 text-2xl font-bold">
